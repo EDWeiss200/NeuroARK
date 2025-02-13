5f562d33291ef166db1194865ff36200d446765d
FeRRRiJane <mykhoedovs@gmail.com>
2025-02-13 11:48:18 +0700

    fix

--- backend/src/services/cancer_service.py
+++ backend/src/services/cancer_service.py
@@ -15,7 +15,7 @@ class CancerServices:
 
         filename = f'{file.filename}'
 
-        x = os.path.join(r'backend/src/services/txt_for_neuro', filename)
+        x = os.path.join(r'services/txt_for_neuro', filename)
 
 
         with open(x, "wb") as file_object:
