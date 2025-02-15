d7e1fe881a4bcbfa293c4ad7638da9ec5d32933f
FeRRRiJane <mykhoedovs@gmail.com>
2025-02-15 11:30:16 +0700

    fix

--- backend/src/utils/neuro/neuro_ark.py
+++ backend/src/utils/neuro/neuro_ark.py
@@ -22,32 +22,27 @@ def load_and_preprocess_image(img_path, target_size=(32, 32)):
     img = image.load_img(img_path, target_size=target_size)
     img_array = image.img_to_array(img)
     img_array = np.expand_dims(img_array, axis=0)
-    img_array = img_array / 255.0  # Нормализация (так же, как в обучении)
+    img_array = img_array / 255.0  
     return img_array
 
-# Укажите путь к вашему изображению
 def neuro_check(image: Image.Image):
-    """
-    Принимает изображение (PIL.Image.Image), выполняет предобработку и предсказывает класс.
-    """
-
-    # Предобработка изображения (адаптировано из load_and_preprocess_image):
+    # Предобработка изображения 
     target_size = (32, 32) # Теперь мы точно знаем размер!
     img = image.resize(target_size)
-    img_array = keras.utils.img_to_array(img) # преобразует PIL image в numpy array
-    img_array = np.expand_dims(img_array, axis=0)  # Добавляем размерность пакета (batch size)
+    img_array = keras.utils.img_to_array(img) 
+    img_array = np.expand_dims(img_array, axis=0)  
     img_array = img_array / 255.0  # Нормализация
 
     # 3. Предсказание класса на основе изображения
     predictions = model.predict(img_array)
     predicted_class = np.argmax(predictions, axis=1)
 
-    # Список названий классов (проверьте, что он совпадает с тем, что использовалось при обучении)
+    # Список названий классов 
     class_names = ['Актинический кератоз', 'Базальноклеточная карцинома', 'Доброкачественные кератозоподобные поражения', 'Дерматофиброма', 'Меланоцитарные невусы', 'Меланома', 'Сосудистые поражения']  # Замените на реальные названия классов
 
     predicted_label = class_names[predicted_class[0]]
-    confidence = predictions[0][np.argmax(predictions, axis=1)[0]]  # Исправлено!
+    confidence = predictions[0][np.argmax(predictions, axis=1)[0]] 
 
     return f"Предсказанный класс: {predicted_label} с уверенностью {confidence*100:.2f}%"
 
-# 4. Отображение изображения с предсказанием
+
