fa0106e59c16a5f248be9b6672f3c45ee2539ad4
FeRRRiJane <mykhoedovs@gmail.com>
2025-04-17 21:06:37 +0700

    up: news_parser 0.1.0

--- backend/src/utils/news_parser.py
+++ backend/src/utils/news_parser.py
@@ -2,16 +2,24 @@ import requests
 from bs4 import BeautifulSoup
 
 
-url = 'https://lenta.ru/search?query=%D0%BE%D0%BD%D0%BA%D0%BE%D0%BB%D0%BE%D0%B3%D0%B8%D1%8F#size=10|sort=2|domain=1|modified,format=yyyy-MM-dd'
+url = 'https://www.gazeta.ru/search.shtml?text=%D0%BE%D0%BD%D0%BA%D0%BE%D0%BB%D0%BE%D0%B3%D0%B8%D1%8F&p=main&input=utf8'
 
 res = requests.get(url,'body')
 
 soup = BeautifulSoup(res.text, "html.parser")
-print(soup.prettify)
 
 
-all_news = soup.find('ul', class_='search-results__list').findAll('li')
-print(all_news)
+
+all_news = soup.find_all('div', class_='b_ear-textblock m_searchresult',limit=10)
+
+result = []
+for i in all_news:
+    div_title = i.find('div', class_='b_ear-title').find('a').next.replace('\n','').replace('\xa0',' ')
+    div_info = i.find('time', class_='b_ear-time m_searchresult').next.replace('\n','').replace('\xa0',' ')
+    result.append([div_title,div_info])
+
+print(result)
+
 
 
 
